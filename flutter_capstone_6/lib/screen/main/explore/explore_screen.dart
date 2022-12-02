731976203b6bae7434293a9654eb5ba559437a46
Angeline Felicia <91734471+angelinefelicia@users.noreply.github.com>
2022-12-02 13:59:08 +0700

    Add topViewCard at explore page

--- flutter_capstone_6/lib/screen/main/explore/explore_screen.dart
+++ flutter_capstone_6/lib/screen/main/explore/explore_screen.dart
@@ -8,131 +8,221 @@ class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: whiteBg,
+      appBar: AppBar(
         backgroundColor: whiteBg,
-        appBar: AppBar(
-          backgroundColor: whiteBg,
-          elevation: 0,
-          title: const Text(
-            'Explore',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              color: n100,
-            ),
+        elevation: 0,
+        title: const Text(
+          'Explore',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: n100,
           ),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.only(left: 25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Search Bar Section
-                const SizedBox(height: 16),
-                searchBarItem(),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.only(left: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Search Bar Section
+              const SizedBox(height: 16),
+              searchBarItem(),
 
-                // Videos Section
-                const SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Videos',
+              // Videos Section
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Videos',
+                    style: TextStyle(
+                      color: n100,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 25),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: n5,
+                    ),
+                    child: const Text(
+                      'See All',
                       style: TextStyle(
-                        color: n100,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: violet,
                       ),
                     ),
-                    Container(
-                      margin: const EdgeInsets.only(right: 25),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 2),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: n5,
-                      ),
-                      child: const Text(
-                        'See All',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: violet,
-                        ),
-                      ),
-                    ),
+                  ),
+                ],
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    videoCard('img1.png', 'Gym from home', '04:52'),
+                    videoCard('img2.png', 'Yoga meditation', '04:12'),
+                    videoCard('img3.png', 'Zumba', '04:56'),
                   ],
                 ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      videoCard('img1.png', 'Gym from home', '04:52'),
-                      videoCard('img2.png', 'Yoga meditation', '04:12'),
-                      videoCard('img3.png', 'Zumba', '04:56'),
-                    ],
-                  ),
-                ),
+              ),
 
-                // Popular Articles Section
-                const SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Popular articles this week',
+              // Popular Articles Section
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Popular articles this week',
+                    style: TextStyle(
+                      color: n100,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 25),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: n5,
+                    ),
+                    child: const Text(
+                      'See All',
                       style: TextStyle(
-                        color: n100,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: violet,
                       ),
                     ),
-                    Container(
-                      margin: const EdgeInsets.only(right: 25),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 2),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: n5,
-                      ),
-                      child: const Text(
-                        'See All',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: violet,
-                        ),
-                      ),
-                    ),
+                  ),
+                ],
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    popularArticlesCard('img4.png',
+                        'Benefits of green tea for metabolism', '1.52k views'),
+                    popularArticlesCard(
+                        'img5.png',
+                        '20-minutes Outdoor Can Make You Less Stress',
+                        '3k views'),
+                    popularArticlesCard(
+                        'img6.png', 'Stress and premature aging', '2.4k views'),
+                    popularArticlesCard(
+                        'img7.png', 'Natrium behind the pack', '1.1k views'),
+                    popularArticlesCard('img8.png',
+                        'Not always bad, there is also good fat', '1.2k views'),
                   ],
                 ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      popularArticlesCard(
-                          'img4.png',
-                          'Benefits of green tea for metabolism',
-                          '1.52k views'),
-                      popularArticlesCard(
-                          'img5.png',
-                          '20-minutes Outdoor Can Make You Less Stress',
-                          '3k views'),
-                      popularArticlesCard('img6.png',
-                          'Stress and premature aging', '2.4k views'),
-                      popularArticlesCard(
-                          'img7.png', 'Natrium behind the pack', '1.1k views'),
-                      popularArticlesCard(
-                          'img8.png',
-                          'Not always bad, there is also good fat',
-                          '1.2k views'),
-                    ],
-                  ),
+              ),
+
+              // Top view Section
+              const SizedBox(height: 24),
+              const Text(
+                'Top view exercise',
+                style: TextStyle(
+                  color: n100,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
                 ),
-              ],
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(right: 25),
+                child: Wrap(
+                  alignment: WrapAlignment.spaceEvenly,
+                  children: [
+                    topViewCard('img9.png', 'Body Workout', '03:12'),
+                    topViewCard('img10.png', 'Planking', '05:52'),
+                    topViewCard('img11.png', 'Zumba', '04:22'),
+                    topViewCard('img12.png', 'Yoga', '04:35'),
+                    topViewCard('img13.png', 'Streching', '04:59'),
+                    topViewCard('img14.png', 'Cardio', '03:48'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget topViewCard(String image, String title, String duration) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 16),
+          width: 158,
+          height: 143,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: AssetImage('assets/explore/$image'),
+              fit: BoxFit.cover,
             ),
           ),
-        ));
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    color: n100.withOpacity(0.3),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 2, horizontal: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: n100,
+                        ),
+                        child: Text(
+                          duration,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+            color: n100,
+          ),
+        ),
+      ],
+    );
   }
 
   Widget popularArticlesCard(String image, String title, String views) {

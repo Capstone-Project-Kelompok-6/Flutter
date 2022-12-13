721578846ba905f4c22942b871ff238b2275d9f8
Kub1ga <arya.batara.sena19@mhs.ubharajaya.ac.id>
2022-12-14 01:51:13 +0700

    improve code

--- flutter_capstone_6/lib/component/repository.dart
+++ flutter_capstone_6/lib/component/repository.dart
@@ -1,36 +1,52 @@
+import 'dart:async';
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class Repository {
-  Future<http.Response> login(String email, String password) {
-    return http.post(
-      Uri.parse('https://www.go-rest-api.live/api/v1/auth/login'),
-      headers: <String, String>{
+  Future<http.Response> login(String email, String password) async {
+    try {
+      var headers = {
         'Content-Type': 'application/json',
         'Authorization':
             'capstoneIb1eP7LctQNIjsbg9J4mzvHll6ap9RtIOCqkaEBHiD60ev0upL9DRp0JDUKEmVfWSiMcZqoUElLfwst4wPQrUAvxvCbeRvt9oNvjkrGSFs3ObOdGSCnwH'
-      },
-      body: jsonEncode(<String, String>{'email': email, 'password': password}),
-    );
+      };
+      var request = await http.post(
+        Uri.parse('https://www.go-rest-api.live/api/v1/auth/login'),
+        body: {"email": email, 'password': password},
+      );
+
+      request.headers.addAll(headers);
+      return request;
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
   }
 
   Future<http.Response> register(String first_name, String last_name,
-      String phone_number, String email, String password) {
-    return http.post(
-        Uri.parse('https://www.go-rest-api.live/api/v1/auth/register'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-          'Authorization':
-              'capstoneIb1eP7LctQNIjsbg9J4mzvHll6ap9RtIOCqkaEBHiD60ev0upL9DRp0JDUKEmVfWSiMcZqoUElLfwst4wPQrUAvxvCbeRvt9oNvjkrGSFs3ObOdGSCnwH'
-        },
-        body: jsonEncode(<String, String>{
-          'first_name': first_name,
-          'last_name': last_name,
-          'phone_number': phone_number,
-          'email': email,
-          'password': password
-        }));
+      String phone_number, String email, String password) async {
+    try {
+      var headers = {
+        'Content-Type': 'application/json',
+        'Authorization':
+            'capstoneIb1eP7LctQNIjsbg9J4mzvHll6ap9RtIOCqkaEBHiD60ev0upL9DRp0JDUKEmVfWSiMcZqoUElLfwst4wPQrUAvxvCbeRvt9oNvjkrGSFs3ObOdGSCnwH'
+      };
+      var request = await http.post(
+          Uri.parse('https://www.go-rest-api.live/api/v1/auth/register'),
+          body: jsonEncode({
+            "first_name": first_name,
+            'last_name': last_name,
+            'phone_number': phone_number,
+            'email': email,
+            'password': password
+          }),
+          headers: headers);
+      return request;
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+    ;
   }
 
   Future<http.Response> sendOtp(String email) async {
@@ -43,9 +59,6 @@ class Repository {
 
       request.headers.addAll(headers);
 
-      // if (request.statusCode == 200) {
-      //   print('object');
-      // }
       return request;
     } catch (e) {
       print(e);
@@ -64,7 +77,6 @@ class Repository {
       request.headers.addAll(headers);
 
       if (request.statusCode == 200) {
-        // print('object');
         return true;
       }
     } catch (e) {
--- flutter_capstone_6/lib/screen/main/video/video_screen.dart
+++ flutter_capstone_6/lib/screen/main/video/video_screen.dart
@@ -29,6 +29,7 @@ class _VideoScreenState extends State<VideoScreen> {
         autoInitialize: true,
         autoPlay: false,
         looping: false,
+        showOptions: true,
         errorBuilder: (context, string) {
           return Center(
               child: Text(

d7e2b4a7750075199947a1b5b167add5233d6969
Angeline Felicia <91734471+angelinefelicia@users.noreply.github.com>
2022-12-14 09:17:07 +0700

    minor update

--- flutter_capstone_6/lib/component/repository.dart
+++ flutter_capstone_6/lib/component/repository.dart
@@ -24,21 +24,28 @@ class Repository {
   }
 
   Future<http.Response> register(String firstName, String lastName,
-      String phoneNumber, String email, String password) {
-    return http.post(
-        Uri.parse('https://www.go-rest-api.live/api/v1/auth/register'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-          'Authorization':
-              'capstoneIb1eP7LctQNIjsbg9J4mzvHll6ap9RtIOCqkaEBHiD60ev0upL9DRp0JDUKEmVfWSiMcZqoUElLfwst4wPQrUAvxvCbeRvt9oNvjkrGSFs3ObOdGSCnwH'
-        },
-        body: jsonEncode(<String, String>{
-          'first_name': firstName,
-          'last_name': lastName,
-          'phone_number': phoneNumber,
-          'email': email,
-          'password': password
-        }));
+      String phoneNumber, String email, String password) async {
+    try {
+      var headers = {
+        'Content-Type': 'application/json',
+        'Authorization':
+            'capstoneIb1eP7LctQNIjsbg9J4mzvHll6ap9RtIOCqkaEBHiD60ev0upL9DRp0JDUKEmVfWSiMcZqoUElLfwst4wPQrUAvxvCbeRvt9oNvjkrGSFs3ObOdGSCnwH'
+      };
+      var request = await http.post(
+          Uri.parse('https://www.go-rest-api.live/api/v1/auth/register'),
+          body: jsonEncode({
+            "first_name": firstName,
+            'last_name': lastName,
+            'phone_number': phoneNumber,
+            'email': email,
+            'password': password
+          }),
+          headers: headers);
+      return request;
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
   }
 
   Future<http.Response> sendOtp(String email) async {
